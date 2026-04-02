25121783b2bd1adfa4637f1c2938a1ad1b6fee56
whyceewhite <ycwhite@gmail.com>
2026-04-01 20:27:27 -0400

    Use Headers as an enum instead of strings to facilitate reordering of columns

--- src/main/java/com/doubletuck/GymScoreParserApplication.java
+++ src/main/java/com/doubletuck/GymScoreParserApplication.java
@@ -1,35 +1,35 @@
 package com.doubletuck;
 
-import com.doubletuck.model.VirtiusScore;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.doubletuck.model.VirtiusScore;
 import com.doubletuck.parser.VirtiusExportStatusWriter;
 import com.doubletuck.parser.VirtiusMeetSessionsParser;
-
-import java.util.List;
 
 @SpringBootApplication
 public class GymScoreParserApplication {
 
-    private final static Logger logger = LoggerFactory.getLogger(GymScoreParserApplication.class);
+  private final static Logger logger = LoggerFactory.getLogger(GymScoreParserApplication.class);
 
-    public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(GymScoreParserApplication.class, args);
+  public static void main(String[] args) {
+    ConfigurableApplicationContext context = SpringApplication.run(GymScoreParserApplication.class, args);
 
-        VirtiusMeetSessionsParser parser = new VirtiusMeetSessionsParser();
-        List<VirtiusScore> virtiusScoreList = parser.getSessionList();
-        logger.info("Virtius scores found: {}", virtiusScoreList.size());
+    VirtiusMeetSessionsParser parser = new VirtiusMeetSessionsParser();
+    List<VirtiusScore> virtiusScoreList = parser.getSessionList();
+    logger.info("Virtius scores found: {}", virtiusScoreList.size());
 
-        logger.info("Writing to an export file.");
-        VirtiusExportStatusWriter writer = new VirtiusExportStatusWriter();
-        writer.create(virtiusScoreList);
-        logger.info("Export completed.");
+    logger.info("Writing to an export file.");
+    VirtiusExportStatusWriter writer = new VirtiusExportStatusWriter();
+    writer.writeFile(virtiusScoreList);
+    logger.info("Export completed.");
 
-        context.close();
-        System.exit(0);
-    }
+    context.close();
+    System.exit(0);
+  }
 }
--- src/main/java/com/doubletuck/parser/VirtiusExportStatusWriter.java
+++ src/main/java/com/doubletuck/parser/VirtiusExportStatusWriter.java
@@ -1,15 +1,20 @@
 package com.doubletuck.parser;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,22 +26,22 @@ public class VirtiusExportStatusWriter {
 
   private static final Path OUTPUT_FILE = Path.of("data/meet_scores_export_status.csv");
 
-  private static final String[] HEADERS = {
-      "meet_date",
-      "meet_name",
-      "score_site",
-      "session_id",
-      "score_url",
-      "is_wag",
-      "export_status",
-      "export_filename",
-      "export_date",
-      "export_message"
-  };
+  private enum Headers {
+    MEET_DATE,
+    MEET_NAME,
+    SCORE_SITE,
+    SESSION_ID,
+    SCORE_URL,
+    WAG_MAG,
+    EXPORT_STATUS,
+    EXPORT_FILENAME,
+    EXPORT_TIMESTAMP,
+    EXPORT_MESSAGE
+  }
 
-  private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+  private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-  public void create(List<VirtiusScore> sessions) {
+  public void writeFile(List<VirtiusScore> sessions) {
     try {
       Files.createDirectories(OUTPUT_FILE.getParent());
     } catch (IOException e) {
@@ -45,16 +50,18 @@ public class VirtiusExportStatusWriter {
     }
 
     try (
-      BufferedWriter writer = Files.newBufferedWriter(
+        BufferedWriter writer = Files.newBufferedWriter(
             OUTPUT_FILE,
             StandardOpenOption.CREATE,
             StandardOpenOption.TRUNCATE_EXISTING);
-      CSVPrinter printer = new CSVPrinter(writer, CSVFormat.DEFAULT.builder().setHeader(HEADERS).get());
-    ) {
+        CSVPrinter printer = new CSVPrinter(writer, CSVFormat.RFC4180.builder()
+            .setHeader(Headers.class)
+            .setRecordSeparator("\n")
+            .get());) {
       int rowCount = 0;
       for (VirtiusScore session : sessions) {
-        List<String> row = buildRow(session);
-        printer.printRecord(row);
+        String[] row = buildRow(session);
+        printer.printRecord((Object[]) row);
         rowCount++;
       }
       printer.flush();
@@ -64,19 +71,67 @@ public class VirtiusExportStatusWriter {
     }
   }
 
-  private List<String> buildRow(VirtiusScore session) {
-    return List.of(
-        session.getMeetDate() == null ? "" : session.getMeetDate().format(DATE_FORMATTER),
-        asNonNullString(session.getMeetName()),
-        "Virtius",
-        asNonNullString(session.getSessionId()),
-        asNonNullString(session.getScoreUrl()),
-        session.isWag() ? "WAG" : "MAG",
-        asNonNullString(session.getExportStatus()),
-        asNonNullString(session.getExportFilename()),
-        session.getExportDate() == null ? "" : session.getExportDate().format(DATE_FORMATTER),
-        asNonNullString(session.getExportMessage())
-        );
+  private String[] buildRow(VirtiusScore session) {
+    String[] row = new String[Headers.values().length];
+    row[Headers.MEET_DATE.ordinal()] = session.getMeetDate() == null ? ""
+        : session.getMeetDate().format(DATE_FORMATTER);
+    row[Headers.MEET_NAME.ordinal()] = asNonNullString(session.getMeetName());
+    row[Headers.SCORE_SITE.ordinal()] = "Virtius";
+    row[Headers.SESSION_ID.ordinal()] = asNonNullString(session.getSessionId());
+    row[Headers.SCORE_URL.ordinal()] = asNonNullString(session.getScoreUrl());
+    row[Headers.WAG_MAG.ordinal()] = session.isWag() ? "WAG" : "MAG";
+    row[Headers.EXPORT_STATUS.ordinal()] = asNonNullString(session.getExportStatus());
+    row[Headers.EXPORT_FILENAME.ordinal()] = asNonNullString(session.getExportFilename());
+    row[Headers.EXPORT_TIMESTAMP.ordinal()] = session.getExportDate() == null ? ""
+        : session.getExportDate().format(DATE_FORMATTER);
+    row[Headers.EXPORT_MESSAGE.ordinal()] = asNonNullString(session.getExportMessage());
+    return row;
+  }
+
+  public List<VirtiusScore> readFile() {
+
+    ArrayList<VirtiusScore> virtiusScoreList = new ArrayList<>();
+
+    if (!Files.exists(OUTPUT_FILE)) {
+      logger.error("Cannot read the Virtius scores export file {} because it does not exist.", OUTPUT_FILE.toString());
+      return virtiusScoreList;
+    }
+
+    try (
+        BufferedReader reader = Files.newBufferedReader(OUTPUT_FILE);
+        CSVParser parser = CSVParser.parse(reader, CSVFormat.RFC4180.builder()
+            .setSkipHeaderRecord(true)
+            .setRecordSeparator("\n")
+            .get());) {
+
+      for (CSVRecord row : parser) {
+        VirtiusScore score = new VirtiusScore();
+        String meetDateStr = row.get(Headers.MEET_DATE.ordinal());
+        if (!meetDateStr.isEmpty()) {
+          score.setMeetDate(LocalDateTime.parse(meetDateStr, DATE_FORMATTER));
+        }
+        score.setMeetName(row.get(Headers.MEET_NAME.ordinal()));
+        score.setSessionId(row.get(Headers.SESSION_ID.ordinal()));
+        score.setScoreUrl(row.get(Headers.SCORE_URL.ordinal()));
+        score.setWag("WAG".equals(row.get(Headers.WAG_MAG.ordinal())));
+        String exportStatusStr = row.get(Headers.EXPORT_STATUS.ordinal());
+        if (!exportStatusStr.isEmpty()) {
+          score.setExportStatus(VirtiusScore.ExportStatus.valueOf(exportStatusStr));
+        }
+        score.setExportFilename(row.get(Headers.EXPORT_FILENAME.ordinal()));
+        String exportDateStr = row.get(Headers.EXPORT_TIMESTAMP.ordinal());
+        if (!exportDateStr.isEmpty()) {
+          score.setExportDate(LocalDateTime.parse(exportDateStr, DATE_FORMATTER));
+        }
+        score.setExportMessage(row.get(Headers.EXPORT_MESSAGE.ordinal()));
+        virtiusScoreList.add(score);
+      }
+
+    } catch (Exception e) {
+      logger.error("An error occurred when reading the Virtius score export status file {}: ", OUTPUT_FILE, e);
+    }
+
+    return virtiusScoreList;
   }
 
   private String asNonNullString(Object value) {
